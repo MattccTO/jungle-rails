ea0e08f16d186ce89a95d4cd01ec041414ffcc0d
Matt Cook <mcc1985@gmail.com>
2019-03-25 20:16:13 +0000

    Items can no longer be added to cart when their stock is < 1

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -4,9 +4,10 @@ class CartsController < ApplicationController
   end
 
   def add_item
-    product_id = params[:product_id].to_s
-    modify_cart_delta(product_id, +1)
-
+    if Product.find(params[:product_id]).quantity > 0
+      product_id = params[:product_id].to_s
+      modify_cart_delta(product_id, +1)
+    end
     redirect_to :back
   end
 
