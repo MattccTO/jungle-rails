0c66bc0922ae4ba31fad4658d2c4431e8b931372
Matt Cook <mcc1985@gmail.com>
2019-03-25 18:24:16 +0000

    Refactor User Validation Specs
    Refactor user validation specs to check error messages on save action and validate based on presence of correct error.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,45 +13,53 @@ RSpec.describe User, type: :model do
 
     it 'should not be valid without a password' do
       subject.password = nil
-      expect(subject).to_not be_valid
+      subject.save
+      expect(subject.errors.full_messages.include? "Password can't be blank").to be true
     end
 
     it 'should not be valid without a password_confirmation' do
       subject.password_confirmation = nil
-      expect(subject).to_not be_valid
+      subject.save
+      expect(subject.errors.full_messages.include? "Password confirmation can't be blank").to be true
     end
 
     it 'should not be valid when the password and password_confirmation are different' do
       subject.password_confirmation = 'notpassword'
-      expect(subject).to_not be_valid
+      subject.save
+      expect(subject.errors.full_messages.include? "Password confirmation doesn't match Password").to be true
     end
 
     it 'should not be valid if email is not unique, case independent' do
       User.destroy_all
       User.create!(first_name: 'timmy', last_name: 'test', email: 'timmy@test.com', password: 'password', password_confirmation: 'password')
       subject.email = 'TIMMY@TEST.com'
-      expect(subject).to_not be_valid
+      subject.save
+      expect(subject.errors.full_messages.include? "Email has already been taken").to be true
     end
 
     it 'should not be valid without an email' do
       subject.email = nil
-      expect(subject).to_not be_valid
+      subject.save
+      expect(subject.errors.full_messages.include? "Email can't be blank").to be true
     end
 
     it 'should not be valid without a first_name' do
       subject.first_name = nil
-      expect(subject).to_not be_valid
+      subject.save
+      expect(subject.errors.full_messages.include? "First name can't be blank").to be true
     end
 
     it 'should not be valid without a last_name' do
       subject.last_name = nil
-      expect(subject).to_not be_valid
+      subject.save
+      expect(subject.errors.full_messages.include? "Last name can't be blank").to be true
     end
 
     it 'should not be valid when the password is too short' do
       subject.password = '1234'
       subject.password_confirmation = '1234'
-      expect(subject).to_not be_valid
+      subject.save
+      expect(subject.errors.full_messages.include? "Password is too short (minimum is 8 characters)").to be true
     end
   end
 end
