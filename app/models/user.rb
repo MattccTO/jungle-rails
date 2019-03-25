bb2599a761f614b5bf6f30a35bf0afef044bbda2
Matt Cook <mcc1985@gmail.com>
2019-03-25 18:41:43 +0000

    Refactor Login Method
    Authentication of a user on login is now performed inside a class method in the User model. The Session Controller calls on this method to authenticate the user and either logs the user in, or doesn't, depending on whether the user credentials are okay.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,13 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 8 }
   validates :password_confirmation, presence: true
+
+  def self.authenticate_with_credentials(email, password)
+    @user = User.find_by(email: email)
+    if @user && @user.authenticate(password)
+      @user
+    else
+      nil
+    end
+  end
 end
