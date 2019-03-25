68798fa87d8159e19f6c3f61d28cdd8bb016ffbc
Matt Cook <mcc1985@gmail.com>
2019-03-25 14:00:46 +0000

    Rspec validation testing for Product model

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    #Initialize a valid product for use in validation testing
+    cat1 = Category.new(name: 'Ethereal')
+    product = cat1.products.new
+    product.name = 'Limited Release Test Kit'
+    product.price = 999.99
+    product.quantity = 1
+
+    it 'should be a valid product' do
+      expect(product).to be_valid
+    end
+
+    it 'is not valid without a name' do
+      product.name = nil
+      expect(product).to_not be_valid
+    end
+
+    it 'is not valid without a price' do
+      product.price = nil
+      expect(product).to_not be_valid
+    end
+
+    it 'is not valid without a qty' do
+      product.quantity = nil
+      expect(product).to_not be_valid
+    end
+
+    it 'is not valid without a category' do
+      product.category = nil
+      expect(product).to_not be_valid
+    end
+  end
+end
